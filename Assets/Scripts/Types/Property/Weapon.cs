38d2a17e4899dcd1f8eb6c5b1bd1f7a3a919eac1
Mustafa Gülsoy <mustafa30045@gmail.com>
2022-09-11 16:19:02 +0300

    First Commit

---
+++ Assets/Scripts/Types/Property/Weapon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    readonly int magnum;
+    readonly string weaponName;
+    readonly string weaponType;
+    readonly string ammoType;
+    int currentMagnum;
+    Animation animator;
+
+}
---
+++ Assets/Scripts/Types/World.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    int id;
+    AudioSource sound;
+}
